1cc0bd85e5e28f84e3c3fc4471c43b7dfd65d74b
Handle Thomas (IT-DLZ) <T.Handle@LZD.saarland.de>
2017-12-07 14:27:47 +0100

    Added Modules to Feature Module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,10 @@ import { AccordionAnchorDirective, AccordionDirective, AccordionLinkDirective } 
 import { ToggleFullscreenDirective } from './directives/fullscreen/toggle-fullscreen.directive';
 import { MenuItemsService } from './services/menu/menu-items.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatCardModule, MatFormFieldModule, MatIconModule, MatTableModule } from '@angular/material';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { CommonModule } from '@angular/common';
+import { LinkModule } from './components/links/link.module';
 
 @NgModule({
   declarations: [
@@ -15,10 +19,16 @@ import { TranslateModule } from '@ngx-translate/core';
     AccordionAnchorDirective,
     AccordionLinkDirective,
     AccordionDirective,
+    CommonModule,
+    LinkModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatTableModule,
+    NgxDatatableModule,
     ToggleFullscreenDirective,
     TranslateModule
   ],
-  imports: [],
   providers: [
     MenuItemsService
   ]
