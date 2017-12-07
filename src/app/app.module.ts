c9eac48e34f308316d686a3de8391bff6babad6c
Handle Thomas (IT-DLZ) <T.Handle@LZD.saarland.de>
2017-12-07 06:17:08 +0100

    Enable Servuce Worker & Route Tracing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -13,8 +14,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    // environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
-    RouterModule.forRoot(appRoutes, /*{ enableTracing: environment.routerTracing } */)
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
+    RouterModule.forRoot(appRoutes, { enableTracing: environment.routerTracing })
   ],
   bootstrap: [
     AppComponent
