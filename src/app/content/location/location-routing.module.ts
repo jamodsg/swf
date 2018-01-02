b5fa4c5f3a970e29ea339b7cdef9fa6fe00fb29e
Handle Thomas (IT-DLZ) <T.Handle@LZD.saarland.de>
2018-01-02 08:49:06 +0100

    added route to edit location

--- src/app/content/location/location-routing.module.ts
+++ src/app/content/location/location-routing.module.ts
@@ -4,6 +4,7 @@ import { LocationsComponent } from './locations/locations.component';
 import { LocationResolver } from './location.resolver';
 import { LocationDetailComponent } from './location-detail/location-detail.component';
 import { LocationMapComponent } from './location-map/location-map.component';
+import { LocationEditComponent } from './location-edit/location-edit.component';
 
 export const routes: Routes = [
   {
@@ -11,13 +12,13 @@ export const routes: Routes = [
     component: LocationsComponent,
     pathMatch: 'full'
   },
-  /*{
+  {
     path: 'edit/:locationId',
     component: LocationEditComponent,
     resolve: {
       location: LocationResolver
     }
-  },*/
+  },
   {
     path: 'detail/:locationId',
     component: LocationDetailComponent,
