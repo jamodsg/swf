cd486e7200cb4d7d9ed18992008a9069b5f5c941
Thomas Handle <thomas.handle@gmail.com>
2018-01-03 07:05:23 +0100

    updating location now works

--- src/app/content/location/location-edit/location-edit.component.ts
+++ src/app/content/location/location-edit/location-edit.component.ts
@@ -5,7 +5,6 @@ import { CategoryTypeService } from '../../../shared/services/category-type/cate
 import { ILocation } from '../../../shared/interfaces/location.interface';
 import { CategoryService } from '../../../shared/services/category/category.service';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MemberService } from '../../../shared/services/member/member.service';
 import { Observable } from 'rxjs/Observable';
 import { ICategory } from '../../../shared/interfaces/category.interface';
 import { ICategoryType } from '../../../shared/interfaces/category-type.interface';
@@ -24,12 +23,11 @@ export class LocationEditComponent implements OnInit {
   public categoryTypes$: Observable<ICategoryType[]>;
 
   constructor(private router: Router,
-    private fb: FormBuilder,
-    private route: ActivatedRoute,
-    public categoryService: CategoryService,
-    public memberService: MemberService,
-    public categoryTypeService: CategoryTypeService,
-    public locationService: LocationService) {
+              private fb: FormBuilder,
+              private route: ActivatedRoute,
+              public categoryService: CategoryService,
+              public categoryTypeService: CategoryTypeService,
+              public locationService: LocationService) {
     this.categories$ = categoryService.categories$;
     this.categoryTypes$ = categoryTypeService.categoryTypes$;
   }
@@ -37,12 +35,11 @@ export class LocationEditComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe((data: { location: ILocation }) => {
       this.location = data.location;
-      // this.uploaderOptions.uploadFolder = this.uploaderOptions.uploadFolder + '/' + this.location.id;
     });
 
     this.form = this.fb.group({
       title: [this.location.title, [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
-      category: [this.location.assignedCategory, [Validators.required]],
+      assignedCategory: [this.location.assignedCategory, [Validators.required]],
       prices: this.location.prices,
       opening: this.location.opening,
       text: this.location.text,
@@ -53,11 +50,10 @@ export class LocationEditComponent implements OnInit {
 
     if (this.location.isImported) {
       this.form.get('title').disable();
-      this.form.get('category').disable();
+      this.form.get('assignedCategory').disable();
       this.form.get('address').disable();
       this.form.get('creation').disable();
     }
-    console.log(this.form);
   }
 
   initAssignedContacts() {
@@ -115,15 +111,18 @@ export class LocationEditComponent implements OnInit {
   saveLocation() {
     let action;
     if (this.location.id) {
-      action = this.locationService.updateLocation(this.location.id, this.location);
+      action = this.locationService.updateLocation(this.location.id, this.form.getRawValue());
     } else {
       action = this.locationService.createLocation(this.location);
     }
-    action.then(() => this.navigateToList());
+    action.then(
+      () => this.navigateToList(),
+      (error: any) => console.log(error)
+    );
   }
 
   navigateToList() {
-    this.router.navigate(['list']).then();
+    this.router.navigate(['/locations']).then();
   }
 
   cancel() {
