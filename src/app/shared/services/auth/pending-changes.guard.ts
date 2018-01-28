8c583d83fcec0d9a972068548048aeab9322e4d8
Thomas Handle <Thomas.Handle@gmail.com>
2018-01-28 23:14:15 +0100

    added german confirm-message

--- src/app/shared/services/auth/pending-changes.guard.ts
+++ src/app/shared/services/auth/pending-changes.guard.ts
@@ -1,4 +1,4 @@
-import { CanDeactivate } from '@angular/router';
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 export interface ComponentCanDeactivate {
@@ -7,14 +7,17 @@ export interface ComponentCanDeactivate {
 
 export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
 
-  canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
+  canDeactivate(component: ComponentCanDeactivate,
+                route: ActivatedRouteSnapshot,
+                state: RouterStateSnapshot): boolean {
     // if there are no pending changes, just allow deactivation; else confirm first
     return component.canDeactivate() ?
       true :
       // NOTE: this warning message will only be shown when navigating elsewhere within your angular app;
       // when navigating away from your angular app, the browser will show a generic warning message
       // see http://stackoverflow.com/a/42207299/7307355
-      confirm('WARNING: You have unsaved changes. Press Cancel to go back and save these changes, or OK to lose these changes.');
+      confirm('ACHTUNG: Du hast nicht gespeicherte Änderungen. Drücke Abbrechen zum Verwerfen dieser Änderungen oder speichere sie ab.');
   }
+
 
 }
