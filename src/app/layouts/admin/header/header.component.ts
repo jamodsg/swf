8f80fa94a636cba27c20739050851d00b68705be
Thomas Handle <Thomas.Handle@gmail.com>
2018-01-29 21:10:33 +0100

    added logOut-function

--- src/app/layouts/admin/header/header.component.ts
+++ src/app/layouts/admin/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
 import * as screenfull from 'screenfull';
+import { AuthService } from '../../../shared/services/auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -11,13 +13,19 @@ export class HeaderComponent {
   @Output() toggleSidenav = new EventEmitter<void>();
   @Output() toggleNotificationSidenav = new EventEmitter<void>();
 
-  constructor() {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   fullScreenToggle(): void {
     if (screenfull.enabled) {
       screenfull.toggle();
     }
+  }
+
+  logOut(){
+    this.authService.signOut().then(
+      () => this.router.navigate([''])
+    );
   }
 
 }
