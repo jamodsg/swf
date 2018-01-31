d6744fc376a26d55476eb479c26e1f5b00c0709e
Thomas Handle <thomas.handle@gmail.com>
2018-01-31 13:13:58 +0100

    removed routing

--- src/app/layouts/admin/admin-routing.ts
+++ src/app/layouts/admin/admin-routing.ts
@@ -11,53 +11,6 @@ export const adminRoutes: Routes = [
         loadChildren: '../../content/dashboard/dashboard.module#DashboardModule'
       },
       {
-        path: 'apps',
-        loadChildren: '../../content/apps/apps.module#AppsModule'
-      },
-      {
-        path: 'widgets',
-        loadChildren: '../../content/widgets/widgets.module#WidgetsModule'
-      },
-      {
-        path: 'material',
-        loadChildren: '../../content/material/material.module#MaterialComponentsModule'
-      },
-      {
-        path: 'ecommerce',
-        loadChildren: '../../content/ecommerce/ecommerce.module#EcommerceModule'
-      },
-      {
-        path: 'taskboard',
-        loadChildren: '../../content/taskboard/taskboard.module#TaskboardModule'
-      },
-      {
-        path: 'forms',
-        loadChildren: '../../content/forms/forms.module#FormModule'
-      },
-      {
-        path: 'tables',
-        loadChildren: '../../content/tables/tables.module#TablesModule'
-      },
-      {
-        path: 'charts',
-        loadChildren: '../../content/chartlib/chartlib.module#ChartlibModule'
-      },
-      {
-        path: 'maps',
-        loadChildren: '../../content/maps/maps.module#MapModule'
-      },
-      {
-        path: 'dragndrop',
-        loadChildren: '../../content/dragndrop/dragndrop.module#DragndropModule'
-      },
-      {
-        path: 'pages',
-        loadChildren: '../../content/pages/pages.module#PagesModule'
-      },
-      ///////////////////
-      // added Modules
-      ///////////////////
-      {
         path: 'articles',
         loadChildren: '../../content/article/article.module#ArticleModule'
       },
