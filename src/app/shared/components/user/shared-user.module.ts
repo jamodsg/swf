4a873cd2a23ebc19a0c0a0cb9a3765411c43dffe
Thomas Handle <thomas.handle@gmail.com>
2018-03-08 09:06:44 +0100

    added sharedUserModule

---
+++ src/app/shared/components/user/shared-user.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
+
+@NgModule({
+  imports: [],
+  declarations: [
+    UserAvatarComponent
+  ],
+  exports: [
+    UserAvatarComponent
+  ],
+  providers: []
+})
+export class SharedUserModule {
+}
